302b5ba8f1f3c7f5d1fb6b9e758b1a725ca52003
javahometech <kammana.hari@gmail.com>
2021-05-13 09:17:46 +0530

    Create tomcatDeploy.groovy

---
+++ vars/tomcatDeploy.groovy
@@ -0,0 +1,10 @@
+def call(cred_id,user,ip){
+sshagent([cred_id]) {
+     // rename the war file
+  sh "mv target/*war target/myweb.war"
+     // copy war file to tomcat server
+  sh "scp -o StrictHostKeyChecking=no target/myweb.war ${user}@${ip}:/opt/tomcat8/webapps"
+     // stop and start tomcat
+  sh "ssh ${user}@${ip} /opt/tomcat8/bin/shutdown.sh"
+  sh "ssh ${user}@${ip} /opt/tomcat8/bin/startup.sh
+}
